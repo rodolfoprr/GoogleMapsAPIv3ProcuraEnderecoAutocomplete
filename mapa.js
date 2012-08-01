e6ee40e2150bf3b2577df17aa6ca2ef0e32678b6
Rodolfo Pereira <rod.lps@hotmail.com>
2012-08-01 12:54:47 -0300

    primeiro
    Exemplo de uso do Google Maps JavaScript API v3 com jQuery UI
    Autocomplete

---
+++ mapa.js
@@ -0,0 +1,104 @@
+var geocoder;
+var map;
+var marker;
+
+function initialize() {
+	var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
+	var options = {
+		zoom: 5,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	
+	map = new google.maps.Map(document.getElementById("mapa"), options);
+	
+	geocoder = new google.maps.Geocoder();
+	
+	marker = new google.maps.Marker({
+		map: map,
+		draggable: true,
+	});
+	
+	marker.setPosition(latlng);
+}
+
+$(document).ready(function () {
+
+	initialize();
+	
+	function carregarNoMapa(endereco) {
+		geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (results[0]) {
+					var latitude = results[0].geometry.location.lat();
+					var longitude = results[0].geometry.location.lng();
+		
+					$('#txtEndereco').val(results[0].formatted_address);
+					$('#txtLatitude').val(latitude);
+                   	$('#txtLongitude').val(longitude);
+		
+					var location = new google.maps.LatLng(latitude, longitude);
+					marker.setPosition(location);
+					map.setCenter(location);
+					map.setZoom(16);
+				}
+			}
+		})
+	}
+	
+	$("#btnEndereco").click(function() {
+		if($(this).val() != "")
+			carregarNoMapa($("#txtEndereco").val());
+	})
+	
+	$("#txtEndereco").blur(function() {
+		if($(this).val() != "")
+			carregarNoMapa($(this).val());
+	})
+	
+	google.maps.event.addListener(marker, 'drag', function () {
+		geocoder.geocode({ 'latLng': marker.getPosition() }, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (results[0]) {  
+					$('#txtEndereco').val(results[0].formatted_address);
+					$('#txtLatitude').val(marker.getPosition().lat());
+					$('#txtLongitude').val(marker.getPosition().lng());
+				}
+			}
+		});
+	});
+	
+	$("#txtEndereco").autocomplete({
+		source: function (request, response) {
+			geocoder.geocode({ 'address': request.term + ', Brasil', 'region': 'BR' }, function (results, status) {
+				response($.map(results, function (item) {
+					return {
+						label: item.formatted_address,
+						value: item.formatted_address,
+						latitude: item.geometry.location.lat(),
+          				longitude: item.geometry.location.lng()
+					}
+				}));
+			})
+		},
+		select: function (event, ui) {
+			$("#txtLatitude").val(ui.item.latitude);
+    		$("#txtLongitude").val(ui.item.longitude);
+			var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
+			marker.setPosition(location);
+			map.setCenter(location);
+			map.setZoom(16);
+		}
+	});
+	
+	$("form").submit(function(event) {
+		event.preventDefault();
+		
+		var endereco = $("#txtEndereco").val();
+		var latitude = $("#txtLatitude").val();
+		var longitude = $("#txtLongitude").val();
+		
+		alert("Endereço: " + endereco + "\nLatitude: " + latitude + "\nLongitude: " + longitude);
+	});
+
+});
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Google Maps API v3: Procura de endereço e Autocomplete
+
+Um pequeno projeto utilizando o Google Maps JavaScript API v3 e jQuery UI Autocomplete para obter a latitude e longitude a partir de um endereço fornecido pelo usuário.
+
+Demo: http://www.princiweb.com.br/demos/google-maps-api-v3-procura-endereco-autocomplete/
+
+jQuery UI Autocomplete: http://jqueryui.com/demos/autocomplete/
+Google Maps JavaScript API v3: https://developers.google.com/maps/documentation/javascript/
